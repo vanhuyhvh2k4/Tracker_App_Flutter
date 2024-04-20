5ecf53dd56346e5d275c5ce5a83678f616535243
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2024-04-21 00:48:43 +0700

    clean architecture

---
+++ packages/expense_repository/lib/src/expense_repo.dart
@@ -0,0 +1,7 @@
+import 'package:expense_repository/expense_repository.dart';
+
+abstract class ExpenseRepository {
+  Future<void> createCategory(Category category);
+
+  Future<List<Category>> getCategory();
+}
